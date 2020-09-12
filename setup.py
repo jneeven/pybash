c013cc36f62f9b57e0d4e1521dfa960d38c870eb
Jelmer <jelmer@plumerai.com>
2020-09-12 21:06:49 +0200

    Refer to the installation dir in bashrc

--- setup.py
+++ setup.py
@@ -5,29 +5,32 @@ from pathlib import Path
 def update_bashrc():
     bashrc = Path.home() / ".bashrc"
     contents = bashrc.read_text()
-    to_write = Path("bashrc_code").read_text()
+    to_write = Path("bashrc_template").read_text()
 
     contents, replacements = re.subn(
-        r"\# \<START OF PYBASHRC CODE\>\n(.|\n)*\n\# \<END OF PYBASHRC CODE\>",
+        r"\# \<START OF PYBASHRC CODE\>\n(.|\n)*\n\# \<END OF PYBASHRC CODE\>\n",
         to_write,
         contents,
     )
     if replacements == 0:
         contents += "\n" + to_write
 
-    bashrc.write_text(contents)
+    bashrc.write_text(
+        contents.replace("<INSTALL_DIR>", str(Path("pybashrc").absolute())),
+    )
     print(f"Modified {bashrc}")
 
 
 if __name__ == "__main__":
     pybashrc_file = Path.home() / ".pybashrc.py"
     if not pybashrc_file.exists():
-        pybashrc_file.write_text((Path("pybashrc") / "pybashrc_default.py").read_text())
+        pybashrc_file.write_text(
+            (Path("pybashrc") / "pybashrc_template.py").read_text()
+        )
         print(f"Created pybashrc file at {pybashrc_file}.")
 
-    alias_file = Path.home() / ".pybashrc_aliases"
-    if not alias_file.exists():
-        alias_file.write_text((Path("pybashrc") / "pybashrc_aliases").read_text())
-        print(f"Created pybashrc alias file at {alias_file}.")
+    alias_file = Path("pybashrc") / ".pybashrc_aliases"
+    alias_file.write_text((Path("pybashrc") / "alias_template").read_text())
+    print(f"Created pybashrc alias file at {alias_file}.")
 
     update_bashrc()
