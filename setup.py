b1a82df8f19e1b8e5843e1af49986c3fe140476b
Jelmer <jelmer@plumerai.com>
2020-09-29 12:51:19 +0200

    Correct version to 1.1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def setup():
 
     setuptools.setup(
         name="pybashrc",
-        version="1.0.1",
+        version="1.1.0",
         author="Jelmer Neeven",
         author_email="author@example.com",
         description="Register python functions as bash commands",
